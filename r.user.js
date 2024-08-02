f5727a960874e4adc804c5ce209b60617b72bc76
JBMagination <26027089+jbmagination@users.noreply.github.com>
2024-08-02 16:25:23 -0400

    Update r.user.js

--- r.user.js
+++ r.user.js
@@ -4,14 +4,22 @@
 // @author      JBMagination
 // @license     AGPL-3.0-or-later OR MIT OR Apache-2.0
 // @description Redirects Exoracer .page.link links to an Exolink instance.
-// @icon        https://r.exoracer.link/icon.png
 // @match       https://exoracer.page.link/*
 // @match       https://exo.page.link/*
+// @icon        https://r.exoracer.link/icon.png
+// @iconURL     https://r.exoracer.link/icon.png
+// @defaulticon https://r.exoracer.link/icon.png
+// @icon64      https://r.exoracer.link/icon64.png
+// @icon64URL   https://r.exoracer.link/icon64.png
 // @supportURL  https://ectoracer.github.io/discord
 // @updateURL   https://r.exoracer.link/r.user.js
 // @downloadURL https://r.exoracer.link/r.user.js
 // @homepageURL https://ectoracer.github.io/exolink
+// @homepage    https://ectoracer.github.io/exolink
+// @website     https://ectoracer.github.io/exolink
+// @source      https://ectoracer.github.io/exolink
 // @grant       GM_listValues
+// @sandbox     DOM
 // @version     1.0.0
 // @run-at      document-start
 // @inject-into content
@@ -22,6 +30,6 @@
 // @compatible  safari
 // ==/UserScript==
 
-addEventListener("beforescriptexecute", s => s.preventDefault());
-let subdomain = (location.pathname == '/' && location.search.startsWith('?')) ? 'long' : (location.host == 'exo.page.link') ? 'exo' : 'exoracer';
-if (new URLSearchParams(new URLSearchParams(location).get('search')).get('d') !== '1') window.location.href = 'https://r.exoracer.link/link/' + subdomain + location.pathname
+var search = new URLSearchParams(location).get('search');
+var subdomain = (location.pathname == '/' && search.startsWith('?')) ? 'long' : (location.host == 'exo.page.link') ? 'exo' : 'exoracer';
+if (new URLSearchParams(search).get('d') !== '1') window.location.href = 'https://r.exoracer.link/link/' + subdomain + location.pathname + search;
